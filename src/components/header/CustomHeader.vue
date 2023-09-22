3ff4c183fc9706116f912bdeb2f7b2c166d8a731
Belymenko Dmytro <dmitry.b@zala.com.au>
2023-09-22 22:12:49 +0300

    Project files

---
+++ src/components/header/CustomHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="bg-[#F3D9DC]">
+        <div class="p-4 max-w-[1325px] m-auto flex items-center justify-between">
+            <router-link to="/" class="font-bold text-3xl">LineStuck</router-link>
+            <div class="flex gap-16">
+                <router-link  v-for="link in navLinks" :to="link.url" :key="link.title">
+                    {{ link.title }}
+                </router-link>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router"
+
+interface NavLink {
+    title: string;
+    url: string;
+}
+
+const navLinks: NavLink[] = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "About",
+        url: "/about"
+    },
+    {
+        title: "Contact",
+        url: "/contact"
+    },
+    {
+        title: "Careers",
+        url: "/careers"
+    }
+]
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import "./index.css";
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
