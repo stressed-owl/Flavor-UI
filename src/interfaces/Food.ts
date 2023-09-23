b473effae03cd9cd026ab89ebb645ee66f8e0d30
Nikolay Ponomarenko <nikolay.pon9912@gmail.com>
2023-09-23 14:30:13 +0300

    Custom interfaces were added. Added a CSS file to turn primevue icons on. Paths for different pages were added

---
+++ src/interfaces/Food.ts
@@ -0,0 +1,5 @@
+export interface Food {
+    id: number;
+    image: string;
+    title: string;
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from "@/views/HomeView.vue"
+import DrinkView from "@/views/categories/DrinkView.vue"
+import AppetizerView from "@/views/categories/AppetizerView.vue"
+import BreakfastView from "@/views/categories/BreakfastView.vue"
+import SnackView from "@/views/categories/SnackView.vue"
+import DessertView from "@/views/categories/DessertView.vue"
+import MyRecipesView from "@/views/categories/MyRecipesView.vue"
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -7,6 +13,41 @@ const routes: Array<RouteRecordRaw> = [
       name: 'Home',
       component: HomeView
     },
+    {
+      path: "/all",
+      name: "All",
+      redirect: "/"
+    },
+    {
+      path: "/drink",
+      name: "Drink",
+      component: DrinkView
+    },
+    {
+      path: "/appetizer",
+      name: "Appetizer",
+      component: AppetizerView
+    },
+    {
+      path: "/breakfast",
+      name: "Breakfast",
+      component: BreakfastView
+    },
+    {
+      path: "/snack",
+      name: "Snack",
+      component: SnackView
+    },
+    {
+      path: "/dessert",
+      name: "Dessert",
+      component: DessertView
+    },
+    {
+      path: '/my_recipes',
+      name: "MyRecipies",
+      component: MyRecipesView
+    }
 ]
 
 const router = createRouter({
